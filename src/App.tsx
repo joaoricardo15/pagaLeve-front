cda50a59bc662e7eb1110963051635913b23b800
Joao Cardoso <joao.cardoso@franq.com.br>
2021-05-02 15:40:56 -0300

    initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/calculator';
 
 function App() {
   return (
-    <div className="App">
+    <div className="hero-section">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <a href="https://www.sinaitechnologies.com/">
+        <img src="https://uploads-ssl.webflow.com/5f5f43d64d4a8a8f0c56d495/5f5f5b9c11b9e62883a6c8d2_sinai_logo_white.svg"></img>
+      </a>
+  
       </header>
+      <div style={{
+          width: "100%",
+          height: 800,
+          justifyContent: 'center',
+          backgroundImage: "url(" + "https://uploads-ssl.webflow.com/5f5f43d64d4a8a8f0c56d495/601820fbb2b99957555cac18_1.png" + ")",
+          backgroundPosition: '50% 100%',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat'
+        }} />
+        <Calculator />
     </div>
   );
 }
